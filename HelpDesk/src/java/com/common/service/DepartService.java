23ccf37b6b504b28b4b99a23631749060ea7c19e
Semen Usov <111semmes111@gmail.com>
2016-02-07 11:08:35 +0300

    5 Service, for classes without "has"

---
+++ HelpDesk/src/java/com/common/service/DepartService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.common.service;
+
+import com.common.model.Deprtment;
+import java.util.List;
+
+/**
+ *
+ * @author SEMEN
+ */
+public interface DepartService {
+    Deprtment addDepart(Deprtment depart);
+    void delete(long id);
+    Deprtment editDepart(Deprtment depart);
+    List<Deprtment> getAll();
+}
---
+++ HelpDesk/src/java/com/common/service/EmployerService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.common.service;
+
+import com.common.model.Employer;
+import java.util.List;
+
+/**
+ *
+ * @author SEMEN
+ */
+public interface EmployerService {
+    Employer addEmployer(Employer empl);
+    void delete(long id);
+    Employer editEmployer(Employer empl);
+    List<Employer> getAll();
+}
