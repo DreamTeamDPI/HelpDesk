12f764cce600e7ff16b12296a2099b103ab934e4
Semen <111semmes111@gmail.com>
2016-03-11 15:28:26 +0300

    made migration

--- HelpDesk/src/com/db/migration/V2_03__moveDBStateToV2.java
+++ HelpDesk/src/com/db/migration/V2_03__moveDBStateToV2.java
@@ -4,16 +4,15 @@ package com.db.migration;
 
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-
 /**
  * Created by Vlad Konoshenko on 11.03.2016.
  */
-public class V2_02__moveDBStateToV2 implements  SpringJdbcMigration {
+public class V2_03__moveDBStateToV2 implements  SpringJdbcMigration {
 
 
     public void migrate(org.springframework.jdbc.core.JdbcTemplate jdbcTemplate) throws Exception {
 
+        //jdbcTemplate.execute("INSERT INTO `helpdesk`.`user` (`firstName`, `lastName`) VALUES ('Flyway', 'Work')");
         jdbcTemplate.execute("create table ss (\n" +
                 "id_user int (10) AUTO_INCREMENT,\n" +
                 "name varchar(20) NOT NULL,\n" +
