4db90c6c8d4250658c0a75a984042f5241df21ea
Konoshenko Vlad <konoshenko.vl@gmail.com>
2016-03-14 16:33:07 +0300

    1

---
+++ HelpDesk/src/com/db/migration/V2_04__moveDBStateToV2.java
@@ -0,0 +1,18 @@
+package com.db.migration;
+
+
+
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+
+/**
+ * Created by Vlad Konoshenko on 11.03.2016.
+ */
+public class V2_04__moveDBStateToV2 implements  SpringJdbcMigration {
+
+
+    public void migrate(org.springframework.jdbc.core.JdbcTemplate jdbcTemplate) throws Exception {
+
+
+    }
+
+}
